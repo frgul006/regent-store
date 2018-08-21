bf1578e757276da51e3bb750696747b5d6e330a3
Fredrik Gullberg <fredrik.gullberg@arbetsformedlingen.se>
2018-08-21 14:18:16 +0200

    Old school impl.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'regent-store';
+
+  todos: { label: string; complete: boolean }[] = [{ label: 'Eat pizza', complete: true }];
+  field: string;
+
+  onClick() {
+    const newTodo = { label: this.field, complete: false };
+    this.todos.push(newTodo);
+    this.field = '';
+  }
 }
