584db12be24abb873b02f3d80bef9bb42d46aea8
Fredrik Gullberg <fredrik.gullberg@arbetsformedlingen.se>
2019-03-04 10:20:16 +0100

    Removed unnecessary code for service workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
@@ -15,7 +14,6 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
     FormsModule,
     MatButtonModule,
